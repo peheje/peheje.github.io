e5778f4f7e1d5a6ffa703fefaab4726a8e23ebf3
Peter Helstrup Jensen <pehj@netcompany.com>
2019-05-29 13:02:50 +0200

    download as csv

--- app.js
+++ app.js
@@ -43,7 +43,44 @@ q("#random-btn").addEventListener("click", function () {
     q("#b").value = listToStr(randomList(100000, 999999, 10000));
 });
 
+q("#download-btn").addEventListener("click", function (e) {
+
+    var a = strToList(q("#a").value);
+    var b = strToList(q("#b").value);
+    var aAndB = strToList(q("#a-and-b").value);
+    var aNotB = strToList(q("#a-not-b").value);
+    var bNotA = strToList(q("#b-not-a").value);
+
+    var data = "Left,Right,In both,Only in left,Only in right\r\n";
+    var max = a.length > b.length ? a.length : b.length;
+
+    for (var i = 0; i < max; i++)
+    {
+        data += takeOrEmpty(a, i);
+        data += ",";
+        data += takeOrEmpty(b, i);
+        data += ",";
+        data += takeOrEmpty(aAndB, i);
+        data += ",";
+        data += takeOrEmpty(aNotB, i);
+        data += ",";
+        data += takeOrEmpty(bNotA, i);
+        data += "\r\n";
+    }
+
+    this.href = "data:text/plain;charset=UTF-8," + encodeURIComponent(data);
+});
+
 // --- FUNCTIONS ---
+
+function takeOrEmpty(list, index) {
+    if (index >= list.length) {
+        return "";
+    }
+    else {
+        return list[index];
+    }
+}
 
 function q(tag) {
     if (tag[0] === "#") {
